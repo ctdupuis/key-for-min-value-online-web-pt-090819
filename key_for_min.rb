36843206e7c46af17ef7db8abb78cc3b59795411
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-19 23:41:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,11 @@
 require 'pry'
 def key_for_min_value(name_hash)
 count = 11
+<<<<<<< HEAD
 key = nil 
+=======
+key = {}
+>>>>>>> bdf4361ca5c1f8206a3dac6d3b6176c3b25d0d22
   name_hash.each do |name, num|
     if num < count
       num = count
